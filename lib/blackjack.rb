bf5517840867e150d3034358e1955220d0b8cdad
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-17 19:53:03 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card = card1
-  deal_card = card2
+  card1 = deal_card
+  card2 = deal_card
   card_total = card1 + card2
   display_card_total(card_total)
   return card_total
